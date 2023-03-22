529b8876f205ee409207b05d5cfcfc94dec2092d
shrutik Nandeshwar <123307004+Shrutik1711@users.noreply.github.com>
2023-03-22 19:55:36 +0530

    Update NumberDigitCounter.java

--- NumberDigitCounter.java
+++ NumberDigitCounter.java
@@ -1,6 +1,6 @@
 public class NumberDigitCounter {
     
-    // method to count the number of digits in a number
+    
     public int countDigits(int number) {
         int count = 0;
         while (number != 0) {
@@ -10,7 +10,7 @@ public class NumberDigitCounter {
         return count;
     }
     
-    // main method to test the NumberDigitCounter class
+    
     public static void main(String[] args) {
         NumberDigitCounter counter = new NumberDigitCounter();
         int number = 123456789;
